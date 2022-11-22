ce386c3f69a900dc263c7ed69429b2b484ee5841
Czerny100 <115626086+Czerny100@users.noreply.github.com>
2022-11-22 22:31:58 +0900

    다른 방법 하나더

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.py
@@ -3,3 +3,12 @@ lines = sys.stdin.readlines()
 for line in lines:
     A, B = map(int, line.split())
     print(A+B)
+
+# 다른 방법
+import sys
+while True:
+    try:
+        A, B = map(int, sys.stdin.readline().split())
+        print(A+B)
+    except :
+        break
